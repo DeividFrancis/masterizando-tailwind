6933d58723ad465b941a724b6fef22b1026266c1
Deivid Francis <deividfrancisdeoliveira@gmail.com>
2023-08-12 00:30:54 -0400

    feat(sidebar) estrutura-do-layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
       },
     },
   },
