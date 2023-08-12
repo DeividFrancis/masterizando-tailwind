25d531425888ed2322471cb52ba28e5046bdf405
Deivid Francis <deividfrancisdeoliveira@gmail.com>
2023-08-12 15:26:16 -0400

    feat(formularios): Campos do Formulário

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ const config: Config = {
     extend: {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
     },
   },
