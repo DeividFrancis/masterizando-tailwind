8a2c828b5410b9f86d973d787c2cc379ea98692c
Deivid Francis <deividfrancisdeoliveira@gmail.com>
2023-08-12 09:44:42 -0400

    feat(formularios): Animação das Abas

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import * as Tabs from '@radix-ui/react-tabs'
+import { motion } from 'framer-motion'
 
 interface TabItemProps {
   value: string
@@ -17,7 +18,10 @@ export function TabItem({ value, title, isSelected }: TabItemProps) {
       <span>{title}</span>
 
       {isSelected && (
-        <div className="absolute -bottom-0.5 left-0 right-0 h-0.5 bg-violet-700" />
+        <motion.div
+          layoutId="tabActive"
+          className="absolute -bottom-0.5 left-0 right-0 h-0.5 bg-violet-700"
+        />
       )}
     </Tabs.Trigger>
   )
